4caa9d8bc1400a62f242759d0a35caf313c9604b
isaacJChen <isaacjc@uw.edu>
2018-04-16 16:31:44 -0700

    disable button when text is empty

--- tipcalc2/app/src/main/java/edu/washington/isaacjc/tipcalc/MainActivity.kt
+++ tipcalc2/app/src/main/java/edu/washington/isaacjc/tipcalc/MainActivity.kt
@@ -6,6 +6,10 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import android.text.Editable
+import android.text.TextWatcher
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +19,31 @@ class MainActivity : AppCompatActivity() {
 
         var input = findViewById(R.id.editText2) as EditText
         val button = findViewById(R.id.button) as Button
+        input.addTextChangedListener(object : TextWatcher {
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.toString().trim { it <= ' ' }.isEmpty()) {
+                    button.isEnabled = false
+                } else {
+                    button.isEnabled = true
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int,
+                                           after: Int) {
+                // TODO Auto-generated method stub
+
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                // TODO Auto-generated method stub
+
+            }
+        })
+
+
+
+
         button.setOnClickListener({
             val amount = input.text.toString()
             val amountWithTip = amount.toDouble()*0.15
